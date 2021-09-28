c0016b8092a4801c40eb389cedb76616e2d8b775
kimjinhwan <hwaneehwanee@rgbsolution.co.kr>
2021-09-28 17:39:59 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// export {
+//     plugins: [
+//         autoprefixer
+//     ]
+// }
+module.exports = {
+    plugins: [
+        require('autoprefixer')
+    ]
+}
---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log('Hello Parcel!')
+
+
+test = () => {
+    return 1234;
+}
+
+console.log(test());
+
+
+let test1= 6;
+
+console.log(test1);
